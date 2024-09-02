6b90c62bc8f4d02b08d028ffc64396c090ff3381
Tesfastion Abraham <tesfitmichael602@gmail.com>
2024-09-02 21:48:58 +0300

    created Main and RectangleArea java clases

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int area;
+        System.out.println("Hello world!");
+        RectangelArea rectangelArea = new RectangelArea();
+        rectangelArea.setLength(4);
+
+        rectangelArea.setWidth(5);
+        area = rectangelArea.getLength() * rectangelArea.getWidth();
+        System.out.println("Area of rectangle " + area);
+    }
+}
+
---
+++ src/RectangelArea.java
@@ -0,0 +1,29 @@
+public class RectangelArea {
+    int length;
+    int width;
+
+    public RectangelArea() {
+    }
+
+    public RectangelArea(int width, int length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
+
